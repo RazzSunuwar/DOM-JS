af4d05012ba0e98e0e7e0fb122252d995945f5c3
Razz Sunuwar <razzkoinch5@gmail.com>
2019-12-26 09:46:55 +0545

    examine of the object

--- doms.js
+++ doms.js
@@ -1,1 +1,16 @@
-console.dir(document);
+// // EXAMINE THE DOCUNEMT OBJECT
+// // console.dir(document);
+// console.log(document.domain);   //To check the domain in developers tools
+// console.log(document.URL);  //To check the URL in developers tools
+// console.log(document.title);    //To check the title in developers tools
+// // document.title = 123;
+// console.log(document.doctype); //To check <!doctype html>
+// console.log(document.head); //To check head section
+// console.log(document.body); //To check body section
+// console.log(document.all);  //To check HTML all collection
+// console.log(document.all[8]);  //To check the index of HTML all collection
+// // document.all[10].textContent = 'Hello'; //Text of index 10 will be change with word 'Hello'
+// console.log(document.forms);    //To check the forms
+// console.log(document.links);    //TO check the links of the page
+
+
